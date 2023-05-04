a43b705df13695ee2516f77ba35a1bd94b83bbbe
Marko Samardzija <marko1716@its.edu.rs>
2023-05-04 23:10:32 +0200

    Frontend components Product & Rating

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div>Ecommerce</div>
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  )
 }
 
 export default App
