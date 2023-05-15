d24fa10dfc5feb2c1df16dac4c26f9fbcd76ac91
Marko Samardzija <marko1716@its.edu.rs>
2023-05-16 00:49:12 +0200

    Product Details Reducer & Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,13 +1,19 @@
 // import { combineReducers, applyMiddleware } from 'redux'
 import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
-import { productListReducer } from './reducers/productReducers'
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducers'
 
 // const reducer = combineReducers({})
 // const initialState = {}
 
 const store = configureStore({
-  reducer: { productList: productListReducer },
+  reducer: {
+    productList: productListReducer,
+    productDetails: productDetailsReducer,
+  },
   preloadedState: {},
   middleware: [thunk],
 })
