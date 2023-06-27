0a498fab4a8c700df89bd8281ebecce5a9ff7389
Marko Samardzija <marko1716@its.edu.rs>
2023-06-27 12:02:21 +0200

    Removing items from cart on logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -26,6 +26,7 @@ import {
   USER_UPDATE_FAIL,
 } from '../constants/userConstants'
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
+import { CART_RESET_ITEM } from '../constants/cartConstants'
 
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -67,6 +68,7 @@ export const logout = () => (dispatch) => {
   dispatch({ type: USER_DETAILS_RESET })
   dispatch({ type: ORDER_LIST_MY_RESET })
   dispatch({ type: USER_LIST_RESET })
+  dispatch({ type: CART_RESET_ITEM })
 }
 
 export const register = (name, email, password) => async (dispatch) => {
